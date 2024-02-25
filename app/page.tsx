762cb65e67a74fb1d35c4037137b84f2a6cd3d8f
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2024-02-25 11:14:40 +0100

    Implement first teaser

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { StartPageSparkles } from "@/components/organisms/StartPageSparkles";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <StartPageSparkles></StartPageSparkles>
+  );
+}
