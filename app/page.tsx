ea596185a9e858a59f12c9fd4590eeb16ecd930e
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2024-03-01 17:20:28 +0100

    Work at website

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { FloatingNavBar } from "@/components/organisms/FloatingNavbar";
 import { StartPageSparkles } from "@/components/organisms/StartPageSparkles";
+import { SvgMaskEffect } from "@/components/organisms/SvgMaskEffect";
 import { WavyBackground } from "@/components/organisms/WavyBackground";
 
 export default function Home() {
@@ -18,7 +19,22 @@ export default function Home() {
         ]}
       ></FloatingNavBar>
       <StartPageSparkles></StartPageSparkles>
-      <WavyBackground>some cool wave</WavyBackground>
+      <WavyBackground>
+        <h1 className="md:text-6xl text-2xl lg:text-5xl font-bold">
+          {"Frontend > Backend"}
+        </h1>
+      </WavyBackground>
+      <SvgMaskEffect
+        revealText={
+          <p className="max-w-4xl mx-auto text-slate-800 text-center  text-4xl font-bold">
+            The first rule of Wöges is, that the backend is schuld
+          </p>
+        }
+        className="h-[40rem]"
+        revealSize={200}
+      >
+        The first rule of Wöges is, that the <span className="text-red-500">backend</span> is <span className="text-red-500">schuld</span>
+      </SvgMaskEffect>
     </>
   );
 }
